9aa12bef5c5b39eeb31ec1d1e08c11024f2803ca
Payam Y.A <50809095+payam-avarwand@users.noreply.github.com>
2023-09-05 02:06:42 +0200

    Update Event-Log-Export.ps1

--- Export.of.EventLogs/Event-Log-Export.ps1
+++ Export.of.EventLogs/Event-Log-Export.ps1
@@ -1,4 +1,4 @@
-﻿# This Script checks if there's a folder titled Devices's HOSTNAME in F-Drive, if not, it will be created, and it will check and do for a subfolder titled "LOGS" inside it, afterwards the current Security Logs will be exported in LOGS.
+﻿# This Script checks if there's a folder titled Devices's HOSTNAME in F-Drive, if not, it will create it, then it will do the same for a subfolder titled "LOGS", afterwards the current Security Logs will be exported in LOGS.
 # We can replace any other Log List name with "Security" to have a current export of that.
 
 # ©Payam Avarwand

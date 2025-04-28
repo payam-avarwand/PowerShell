f73d9d3aea1e0ef07dbae4e966966b780b5664cc
Payam_Avarwand <Payam_Avar@yahoo.com>
2025-04-28 03:56:09 +0200

    SSCM

---
+++ Configs and Tools/SSCM/SSCM - Runner.bat
@@ -0,0 +1,37 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyHcjLQlHcDSDJWq3OpEZ++Pv4Pq7kX4+ed4MS6z2lKSdcYA=
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFCtVVQWPAE+/Fb4I5/jH3OKGsEQTU94MS6z25oe9A81e40bre9gk1XU6
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF65
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF65
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpSI=
+::egkzugNsPRvcWATEpSI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAjk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHGQ=
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQIDLRdSATeLO2W/AvU/7frv5uyOoUwJUaISeYHa3bGcQA==
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDDexH0ba
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWHiS9VAnKRVQLA==
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATExEMpKRYUbRKDLny7CLF8
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFCtVVQWPAE+/Fb4I5/jH/PmUsQMYTOdf
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+PowerShell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File "SSCM.ps1"
+exit
